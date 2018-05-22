2be92e617662dd63151d9c9df65232147202a2a4
Wenzhe Ding <wding@elementum.com>
2018-05-21 20:14:44 -0700

    finalize updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -52,18 +52,17 @@ class WaypointUpdater(object):
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             if self.pose is not None and self.waypoints_tree is not None:
-                closest_waypoint_i = self.get_closest_waypoint_id()
-                self.publish_waypoints(closest_waypoint_i)
+                self.publish_waypoints()
             rate.sleep()
 
     def pose_cb(self, msg):
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
+        # DONE2: Implement
         self.base_waypoints = waypoints
         self.waypoints_2d = [
-            (waypoint.pose.pose.position.x,
+            (waypoint.pos2e.pose.position.x,
              waypoint.pose.pose.position.y)
             for waypoint in waypoints.waypoints
         ]
@@ -71,8 +70,8 @@ class WaypointUpdater(object):
         assert self.waypoints_tree is not None
 
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        # DONE: Callback for /traffic_waypoint message. Implement
+        self.traffic_light_data = msg
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
@@ -111,11 +110,35 @@ class WaypointUpdater(object):
             closest_i %= len(self.waypoints_2d)
         return closest_i
 
-    def publish_waypoints(self, i):
+    def publish_waypoints(self):
+        final_lane = self.generate_lane()
+        self.final_waypoints_pub.publish(final_lane)
+
+    def generate_lane(self):
         lane = Lane()
-        lane.header = self.base_waypoints.header
-        lane.waypoints = self.base_waypoints.waypoints[i:i + LOOKAHEAD_WPS]
-        self.final_waypoints_pub.publish(lane)
+        closest_idx = self.get_closest_waypoint_id()
+        farthest_idx = closest_idx + LOOKAHEAD_WPS
+        base_waypoints = self.base_lane.waypoints[closest_idx: farthest_idx]
+
+        if self.stopline_wp_idx == -1 or (self.stopline_wp_idx >= farthest_idx):
+            lane.waypoints = base_waypoints
+        else:
+            lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_idx)
+        return lane
+
+    def decelerate_waypoints(self, waypoints, closest_idx):
+        temp = []
+        for i, wp in enumerate(waypoints):
+            p = Waypoint()
+            p.pose = wp.pose_cb
+            stop_idx = max(self.stopline_wp_idx - closest_idx - 2,0)
+            dist = self.distance(waypoints, i, stop_idx)
+            vel = math.sqrt(2 * MAX_DECEL * dist)
+            if vel < 1.:
+                vel = 0.
+            p.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
+            temp.append(p)
+        return temp
 
 
 if __name__ == '__main__':
