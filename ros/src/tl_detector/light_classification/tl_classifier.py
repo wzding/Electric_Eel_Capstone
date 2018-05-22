181d21ec1fb6aa9c793635c390f8c514de84def3
Wenzhe Ding <wding@elementum.com>
2018-05-21 20:14:20 -0700

    added classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,48 @@
 from styx_msgs.msg import TrafficLight
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+from collections import defaultdict
+from io import StringIO
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+import time
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.current_light = TrafficLight.UNKNOWN
+        PATH_TO_LABELS = 'final/sdc_label_map.pbtxt'
+        GRAPH_FILE = 'final/frozen_inference_graph.pb'
+        # only 4 classes are specified
+        NUM_CLASSES = 4
+        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(label_map,
+            max_num_classes=NUM_CLASSES, use_display_name=True)
+        self.category_index = label_map_util.create_category_index(categories)
+        # initialize
+        self.detection_graph = tf.Graph()
+        # https://github.com/tensorflow/tensorflow/issues/6698
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        with self.detection_graph.as_default():
+            graph_def = tf.GraphDef()
+            with tf.gfile.GFile(GRAPH_FILE, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(graph_def, name='')
+            self.sess = tf.Session(graph=self.detection_graph, config=config)
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+        print("Loaded graph")
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +54,41 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        image_np_expanded = np.expand_dims(image, axis=0)
+        #  detection.
+        with self.detection_graph.as_default():
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.detection_boxes, self.detection_scores,
+                self.detection_classes, self.num_detections],
+                feed_dict={self.image_tensor: image_np_expanded})
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+        # set a threshold
+        min_score_thresh = .50
+            for i in range(boxes.shape[0]):
+                if scores is None or scores[i] > min_score_thresh:
+                    class_name = self.category_index[classes[i]]['name']
+                    if class_name == 'Red':
+                        self.current_light = TrafficLight.RED
+                    elif class_name == 'Green':
+                        self.current_light = TrafficLight.GREEN
+                    elif class_name == 'Yellow':
+                        self.current_light = TrafficLight.YELLOW
+                    # get distance to traffic light
+                    perceived_width_x = (boxes[i][3] - boxes[i][1]) * 800
+                    perceived_width_y = (boxes[i][2] - boxes[i][0]) * 600
+                    # traffic light is 3 feet long and 1 foot wide
+                    fx =  1345.200806
+                    fy =  1353.838257
+                    perceived_depth_x = ((1 * fx) / perceived_width_x)
+                    perceived_depth_y = ((3 * fy) / perceived_width_y )
+                    estimated_distance = round((perceived_depth_x + perceived_depth_y) / 2)
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image, boxes, classes, scores,
+            self.category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8)
+        self.image_np_deep = image
+        return self.current_light
