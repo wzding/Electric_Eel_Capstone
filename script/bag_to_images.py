0ef00d4070461bcb33dc6c637e6a779ae6fe70ef
Wenzhe Ding <wding@elementum.com>
2018-06-12 16:50:58 -0700

    added prefix to file name

--- script/bag_to_images.py
+++ script/bag_to_images.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Copyright 2016 Massachusetts Institute of Technology
-
-"""Extract images from a rosbag.
+"""
+Extract images from a rosbag.
+python bag_to_images.py --bag_file just_traffic_light.bag --output_dir images/  --file_prefix just_traffic_light_
+python bag_to_images.py --bag_file loop_with_traffic_light.bag --output_dir images/  --file_prefix loop_with_traffic_light_
 """
 import os
 import argparse
@@ -13,11 +13,13 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 parser = argparse.ArgumentParser(description="Extract images from a ROS bag.")
-parser.add_argument("bag_file", help="Input ROS bag.")
-parser.add_argument("output_dir", help="Output directory.")
+parser.add_argument("--bag_file", help="Input ROS bag.")
+parser.add_argument("--output_dir", help="Output directory.")
+parser.add_argument("--file_prefix", help="file prefix.")
 args = parser.parse_args()
 bag_file = args.bag_file
 output_dir = args.output_dir
+file_prefix = args.file_prefix
 
 def get_image_topic(bag):
     info = bag.get_type_and_topic_info()
@@ -28,7 +30,7 @@ def get_image_topic(bag):
 def main():
     """Extract a folder of images from a rosbag.
     """
-    print "Extract images from %s into %s" % (bag_file, output_dir)
+    print("Extract images from %s into %s" % (bag_file, output_dir))
     bridge = CvBridge()
     count = 0
     bag = rosbag.Bag(bag_file, "r")
@@ -38,8 +40,8 @@ def main():
         # color image ??
         # cv2.imwrite(os.path.join(output_dir, "frame%06i.png" % count),
         #     cv2.cvtColor(cv_img, cv2.COLOR_GRAY2RGB))
-        cv2.imwrite(os.path.join(output_dir, "frame%06i.png" % count), cv_img)
-        print "Wrote image %i" % count
+        cv2.imwrite(os.path.join(output_dir, file_prefix + "%04i.jpeg" % count), cv_img)
+        print("Wrote image %i" % count)
         count += 1
     bag.close()
     return
