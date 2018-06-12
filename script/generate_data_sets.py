5339d3973205d0feefd846b076183c4a8682b8b5
Wenzhe Ding <wding@elementum.com>
2018-06-12 16:51:35 -0700

    used pandas to read csv file

--- script/generate_data_sets.py
+++ script/generate_data_sets.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-
+"""
+this script needs data denotes the path of a image and the color of traffic light in the image
+it uses object detection API to detect the bounding boxes along with the above data to generate .record file
+"""
 import argparse
 import numpy as np
+import pandas as pd
 import os
 import six.moves.urllib as urllib
 import sys
@@ -9,7 +13,6 @@ import tarfile
 import tensorflow as tf
 import zipfile
 import six
-
 from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
@@ -18,39 +21,55 @@ from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 from object_detection.utils import dataset_util
 
-textlabel = [ 'RED', 'YELLOW', 'GREEN' ]
-label = [ 1, 2, 3 ]
+parser = argparse.ArgumentParser(description='Traffic Light CSV to TFRecord file format converter')
+parser.add_argument('--input_file', type=str, default='data/loop_with_traffic_light.csv', help='traffic light csv input file')
+parser.add_argument('--output_file', type=str, default='data/tl_train.record', help='TFRecord file')
+parser.add_argument('--label_map', type=str, default='data/mscoco_label_map.pbtxt', help='label map file')
+parser.add_argument('--new_label_map', type=str, default='data/sdc_label_map.pbtxt', help='label map file')
+parser.add_argument('--model_dir', type=str, default='models/faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28', help='model directory')
+args = parser.parse_args()
+MODEL_DIR = args.model_dir
+# Path to frozen detection graph. This is the model that is used for finding the bounding box
+# of the traffic light in the image
+PATH_TO_CKPT = MODEL_DIR + '/frozen_inference_graph.pb'
+# List of the strings that is used to add correct label for each box.
+LABEL_MAP = args.label_map
+NEW_LABEL = args.new_label_map
+INPUT_FILE = args.input_file
+OUTPUT_FILE = args.output_file
+# incsv = os.getcwd()+'data/'+ args.infilename
 
 def encode_image_array_as_jpg_str(image):
-  """Encodes a numpy array into a JPEG string.
+    """Encodes a numpy array into a JPEG string.
 
-  Args:
+    Args:
     image: a numpy array with shape [height, width, 3].
 
-  Returns:
+    Returns:
     JPEG encoded image string.
-  """
-  image_pil = Image.fromarray(np.uint8(image))
-  output = six.BytesIO()
-  image_pil.save(output, format='JPEG')
-  jpg_string = output.getvalue()
-  output.close()
-  return jpg_string
+    """
+    image_pil = Image.fromarray(np.uint8(image))
+    output = six.BytesIO()
+    image_pil.save(output, format='JPEG')
+    jpg_string = output.getvalue()
+    output.close()
+    return jpg_string
 
 def create_record(box, newclass, image, filepath):
+    textlabel = [ 'RED', 'YELLOW', 'GREEN' ]
+    label = [ 1, 2, 3 ]
     height = image.shape[0]
     width = image.shape[1]
-    filename = filepath
+    filename = filepath.encode()
     encoded_image_data = encode_image_array_as_jpg_str(image)
     image_format = b'jpeg'
-
     xmins = [ box[1] ] # List of normalized left x coordinates in bounding box (1 per box)
     xmaxs = [ box[3] ] # List of normalized right x coordinates in bounding box
     # (1 per box)
     ymins = [ box[0] ] # List of normalized top y coordinates in bounding box (1 per box)
     ymaxs = [ box[2] ] # List of normalized bottom y coordinates in bounding box
     # (1 per box)
-    classes_text = [ textlabel[newclass] ] # List of string class name of bounding box (1 per box)
+    classes_text = [ textlabel[newclass].encode() ] # List of string class name of bounding box (1 per box)
     classes = [ label[newclass] ] # List of integer class id of bounding box (1 per box)
 
     tf_record = tf.train.Example(features=tf.train.Features(feature={
@@ -74,70 +93,37 @@ def load_image_into_numpy_array(image):
     return np.array(image.getdata()).reshape(
         (im_height, im_width, 3)).astype(np.uint8)
 
-# What model to download.
-MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the model that is used for finding the bounding box
-# of the traffic light in the image
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-
-# List of the strings that is used to add correct label for each box.
-PATH_TO_PRETRAIN_LABELS = './research/object_detection/data/mscoco_label_map.pbtxt'
-NUM_CLASSES = 90
-TRAFFIC_LIGHT_CLASS = 10
-THRESHOLD = 0.89
-PATH_TO_NEW_LABLES = './research/object_detection/data/sdc_label_map.pbtxt'
-
-# already load pre-train model and weights?
-if not os.path.exists('faster_rcnn_resnet101_coco_11_06_2017/model.ckpt.index'):
-    if not os.path.exists('faster_rcnn_resnet101_coco_11_06_2017.tar.gz'):
-        print "Downloading Pre-trained model and weights..."
-        opener = urllib.request.URLopener()
-        opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-    tar_file = tarfile.open(MODEL_FILE)
-    print "Extracting Pre-trained model and weights..."
-    for file in tar_file.getmembers():
-        tar_file.extract(file, os.getcwd())
-    print "Done!"
-
-# load the model into memory
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
-label_map = label_map_util.load_labelmap(PATH_TO_PRETRAIN_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
-
-
 if __name__ == '__main__':
-    defaultOutput = 'tl_train.record'
-    parser = argparse.ArgumentParser(description='Udacity SDC: System Integration - Traffic Light CSV to TFRecord file format converter')
-    parser.add_argument('--infilename', type=str, default='loop_with_traffic_light.csv', help='traffic light csv input file')
-    parser.add_argument('--outfilename', type=str, default=defaultOutput, help='TFRecord file')
-    args = parser.parse_args()
-    incsv = os.getcwd()+'data/'+args.infilename
-
-    # read in incsv file
-    imagefiles = []
-    newlabels = []
-    with open(incsv) as f:
-        count = 0
-        for line in f:
-            if count > 0:
-                data = line.split(',')
-                imagefiles.append(os.getcwd()+data[7].replace("'", ""))
-                newlabels.append(int(data[8]))
-            count += 1
-
+#     MODEL_FILE = MODEL_NAME + '.tar.gz'
+#     DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+    # already load pre-train model and weights?
+#     if not os.path.exists('faster_rcnn_resnet101_coco_11_06_2017/model.ckpt.index'):
+#         if not os.path.exists('faster_rcnn_resnet101_coco_11_06_2017.tar.gz'):
+#             print "Downloading Pre-trained model and weights..."
+#             opener = urllib.request.URLopener()
+#             opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+#         tar_file = tarfile.open(MODEL_FILE)
+#         print "Extracting Pre-trained model and weights..."
+#         for file in tar_file.getmembers():
+#             tar_file.extract(file, os.getcwd())
+#         print "Done!"
+    NUM_CLASSES = 90
+    TRAFFIC_LIGHT_CLASS = 10
+    THRESHOLD = 0.89
+    # load the model into memory
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+    label_map = label_map_util.load_labelmap(LABEL_MAP)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
     # start writing to the record file
-    writer = tf.python_io.TFRecordWriter(args.outfilename)
+    writer = tf.python_io.TFRecordWriter(OUTPUT_FILE)
+    # detecting and saving files
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
             # Definite input and output Tensors for detection_graph
@@ -149,9 +135,13 @@ if __name__ == '__main__':
             detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
             detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-            for i in range(len(imagefiles)):
-                if newlabels[i] < 3:     # don't try to classify 'unknown'
-                    image = Image.open(imagefiles[i])
+            # read file
+            df = pd.read_csv(INPUT_FILE)
+            for index, row in df.iterrows():
+                img = row['image_path'].replace("'", "")
+                l = row['label']
+                if l < 3:     # don't try to classify 'unknown'
+                    image = Image.open(img)
                     # the array based representation of the image will be used later in order to prepare the
                     # result image with boxes and labels on it.
                     image_np = load_image_into_numpy_array(image)
@@ -168,7 +158,9 @@ if __name__ == '__main__':
                     for j in range(len(classes)):
                         if classes[j] == TRAFFIC_LIGHT_CLASS:
                             if scores[j] is not None and scores[j] > THRESHOLD:
-                                print "writing record:", i, imagefiles[i], boxes[j], "score:", scores[j], "new label:", newlabels[i]
-                                record = create_record(boxes[j], newlabels[i], image_np, imagefiles[i])
+                                print("writing record:", index, img,
+                                      "score:", scores[j],
+                                      "new label:", l)
+                                record = create_record(boxes[j], l, image_np, img)
                                 writer.write(record.SerializeToString())
     writer.close()
